0357499f3cff85bfbb434e2397cdcf398f96549e
akul2203 <gehlotakul@gmail.com>
2024-07-04 18:21:19 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+
+Memory updated
+Here is the updated portfolio content for Akul Gehlot:
+
+<h2 align="center">
+  Portfolio Website - v2.0<br/>
+  <a href="https://akulgehlot.tech" target="_blank">akulgehlot.tech</a>
+</h2>
+<div align="center">
+  <img alt="Demo" src="./Images/readme-img1.png" />
+</div>
+<br/>
+<center>
+  
+  
+  
+  
+
+</center>
+<h3 align="center">
+    🔹
+    <a href="https://github.com/akulgehlot/Portfolio/issues">Report Bug</a> &nbsp; &nbsp;
+    🔹
+    <a href="https://github.com/akulgehlot/Portfolio/issues">Request Feature</a>
+</h3>
+TL;DR
+You can fork this repo to modify and make changes of your own. Please give proper credit by linking back to Akul Gehlot. Thanks!
+
+Built With
+My personal portfolio <a href="https://akulgehlot.tech" target="_blank">akulgehlot.tech</a> which features some of my GitHub projects as well as my resume and technical skills.<br/>
+
+This project was built using these technologies.
+
+React.js
+Node.js
+Express.js
+CSS3
+VsCode
+Vercel
+Features
+📖 Multi-Page Layout
+
+🎨 Styled with React-Bootstrap and CSS with easy-to-customize colors
+
+📱 Fully Responsive
+
+Getting Started
+Clone down this repository. You will need node.js and git installed globally on your machine.
+
+🛠 Installation and Setup Instructions
+Installation: npm install
+
+In the project directory, you can run: npm start
+
+Runs the app in the development mode.
+Open http://localhost:3000 to view it in the browser.
+The page will reload if you make edits.
+
+Usage Instructions
+Open the project folder and navigate to /src/components/. <br/>
+You will find all the components used, and you can edit your information accordingly.
+
+Show your support
+Give a ⭐ if you like this website!
+
+<a href="https://www.buymeacoffee.com/akulgehlot" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-violet.png" alt="Buy Me A Coffee" height= "60px" width= "217px" ></a>
+
+Akul Gehlot's Details
+Indore 452009, India
+22/01/2003
+gehlotakul@gmail.com
+GitHub | LinkedIn
++91 7978363646
---
+++ src/components/Footer.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Akul Gehlot</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} AG</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/akul2203"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://x.com/gehlotakul09?t=LjgNHfexXOvi6tp329S0fg&s=09"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="www.linkedin.com/in/akul-gehlot-bb2190232"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="mailto:gehlotakul09@gmail.com"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+                type="email"
+              >
+                <AiOutlineMail />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/akul_santosh_gehlot?igsh=MWM5NWJ1czRlbzJ6MQ=="
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
